f57ce63642b862ea8590a7185c8325c449549e77
Rafaelo <gjergjirafo@gmail.com>
2024-01-14 19:04:36 -0500

    complete review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Rafaelo Gjergji';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = 'Welcome, R!';
